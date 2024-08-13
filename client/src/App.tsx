eadbdad621674f0899c765cf036d910242973753
deadlock1190 <139451287+deadlock1190@users.noreply.github.com>
2024-08-13 19:49:31 +0530

    Created dev branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className='app'>
       <ThemeProvider theme = {theme}>
         <CssBaseline/>
-        hi
+        Checking this branch
       </ThemeProvider>
     </div>
   )
