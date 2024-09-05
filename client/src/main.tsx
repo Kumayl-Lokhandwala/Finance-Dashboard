ce3797c9a4518ac89ab037efc4fba37a0b97e884
deadlock1190 <139451287+deadlock1190@users.noreply.github.com>
2024-09-05 13:05:09 +0530

    Added server side and worked on Row 1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { api } from './state/api.ts'
+import { red } from '@mui/material/colors'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {  [api.reducerPath]: api.reducer },
+  middleware:(getDefault) => getDefault().concat(api.middleware),
+})
+
+setupListeners(store.dispatch)
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
